a5808ac3538c1fc62d371c826c94fafd9badc28e
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-07 16:21:53 +0700

    Đã xong thực hành 2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+namespace UsingOperator
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            float Width;
+            float Height;
+            Console.WriteLine("Nhap gia tri cho hinh chu nhat");
+            Console.WriteLine("Nhap gia tri chieu rong");
+            Width = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap gia tri chieu cao");
+            Height = float.Parse(Console.ReadLine());
+            Console.WriteLine("Dien tich hinh chu nhat la:" +  Width*Height);
+        }
+    }
+}
